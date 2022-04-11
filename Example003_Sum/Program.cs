0090e9b0304d8680f80eb92a3d1d9c482f25c4d7
Helen <e.xolodkova@inbox.ru>
2022-04-11 23:27:51 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Сложить 2 числа
-int numberA = 10;
+int numberA = new Random().Next(1,10); //1,2,3...9
 Console.WriteLine(numberA);
-int numberB = -2;
+int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.Write("Сумма чисел: ");
