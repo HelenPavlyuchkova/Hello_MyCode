7607e007a50fc99537ae18143771494661425914
Helen <e.xolodkova@inbox.ru>
2022-04-11 23:02:36 +0300

    add Example004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿// Деление двух чисел
+double numberA = 10;
+double numberB = 8;
+double result = numberA / numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine("Частное чисел: ");
+Console.WriteLine(result);
