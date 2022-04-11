e931a3b6ca28b60e8a8e87c390af9de7ee5e95a6
Helen <e.xolodkova@inbox.ru>
2022-04-11 22:11:17 +0300

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// Сложить 2 числа
+int numberA = 10;
+Console.WriteLine(numberA);
+int numberB = -2;
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("Сумма чисел: ");
+Console.WriteLine(result);
