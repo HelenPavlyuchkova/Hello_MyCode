eb61a138c557e28c73d304406f583b0a90b62744
Helen <e.xolodkova@inbox.ru>
2022-05-29 11:45:11 +0300

    add exother to lection 6

---
+++ exother/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"//дан текст
+                .Replace("(", "")//замените в нем скобки
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ")//возьми текст, разбей его
+                .Select(item => item.Split(','))//сделай выборку по запятой
+                .Select(e => (x: int.Parse(e[0]),y: int.Parse(e[1])))//сделай выборку так, чтобы первая и вторая координаты конвертировали в числа
+                .Where(e => e.x % 2 == 0)//выбираем те координаты, в кот. первая координата четная
+                .Select(point => (point.x * 10, point.y))//преобразовать первую координату(*10)
+                .ToArray();//превратили это в массив
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+    // for (int k = 0; k < data[i].Length; k++)
+    // {
+    //     Console.WriteLine(data[i][k]);
+    // }
+    //Console.WriteLine();
+}
